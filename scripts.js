ba7c686a01275b45091afae81003c6f2b4b001db
rochadiegox <127871082+rochadiegox@users.noreply.github.com>
2023-08-23 20:18:17 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,20 @@
+const backToTopBtn = document.querySelector("#back-to-top");
+
+window.addEventListener("scroll", function () {
+    const scrolltop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrolltop > 500) {
+        backToTopBtn.style.display = "block";
+    } else {
+        backToTopBtn.style.display = "none";
+    }
+});
+
+backToTopBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+});
